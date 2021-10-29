7d294cfaa033ff05971c13d9f1b266d9c7912e27
Qingdong Cheng <david.mike.cheng@gmail.com>
2021-10-29 15:34:44 -0400

    Create 1416. Restore The Array.java

---
+++ 1416. Restore The Array.java
@@ -0,0 +1,31 @@
+class Solution {
+    private final int M = 1000000007;
+    public int numberOfArrays(String s, int k) {
+        int n = s.length();
+        Map<Integer, Integer> dp = new HashMap<>();
+        
+        return helper(s, 0, k, dp);
+    }
+    
+    private int helper(String s, int start, int k, Map<Integer, Integer> dp) {
+        if (start == s.length()) {
+            return 1;
+        }
+   
+        if (s.charAt(start) == '0') {
+            return 0;
+        }
+        if (dp.containsKey(start)) {
+            return dp.get(start);
+        }
+        long num = 0;
+        int ans = 0;
+        for (int j = start + 1; j <= s.length(); j++) {
+            num = num * 10 + (s.charAt(j - 1) - '0');
+            if (num > k) break;
+            ans = ans + (helper(s, j, k, dp)) % M;
+        }
+        dp.put(start, ans);
+        return ans;
+    }
+}
