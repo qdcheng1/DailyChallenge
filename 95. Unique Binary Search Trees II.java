ae569db2201ae277a61a0b413da5459f13cb86f2
Qingdong Cheng <david.mike.cheng@gmail.com>
2021-11-22 21:55:41 -0500

    Create 95. Unique Binary Search Trees II.java

---
+++ 95. Unique Binary Search Trees II.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return helper(1, n);
+    }
+    
+    private List<TreeNode> helper(int start, int end) {
+        List<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftRes = helper(start, i - 1);
+            List<TreeNode> rightRes = helper(i + 1, end);
+            //左边如果有三种情况，右边有两种情况，那会产生3 * 2种不同的bst
+            for (TreeNode l : leftRes) {
+                for (TreeNode r : rightRes) {
+                    TreeNode cur = new TreeNode(i);
+                    cur.left = l;
+                    cur.right = r;
+                    res.add(cur);
+                }
+            }
+        }
+        return res;
+    }
+}
